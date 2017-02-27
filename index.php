7cb59d1bc74f675b8b49a2dfe3e58282cf891a4d
Riley Hein <Riley.Hein@rizepoint.com>
2017-02-27 09:52:51 -0700

    files for Riley Hein Portfolio

---
+++ index.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Riley Hein | Portfolio</title>
+<!--insert normalize-->
+<!--insert jQuery-->
+<link href="/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="container-fluid">
+	<div class="navbar">
+			<div class="col-md-4, col-sm-6, col-xs-12">
+				<ul class="nav nav-tabs">
+  					<li role="presentation" class="active"><a href="#">Home</a></li>
+  					
+  					<li role="presentation" class="dropdown">
+  					<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+      					Portfolio <span class="caret"></span>
+    				</a>
+    				<ul class="dropdown-menu portfolio-menu">
+    					<li><a href="#">Web</a></li>
+    					<li><a href="#">Graphic Design</a></li>
+    					<li><a href="#">Traditional Art</a></li>
+    					<li><a href="#">Video</a></li>
+     
+    				</ul>
+  					</li>
+
+  					
+  					<li role="presentation" class="dropdown">
+  					<li role="presentation" class="dropdown">
+  					<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+      					About <span class="caret"></span>
+    				</a>
+    				<ul class="dropdown-menu about-menu">
+    					<li><a href="#">About</a></li>
+    					<li><a href="#">Resume</a></li>
+    					<li><a href="#">Contact</a></li>
+     
+    				</ul>
+  					</li>
+  					
+  					<li role="presentation"><a href="#">Shop</a></li>
+ 
+				<ul>
+			</div>
+	</div><!--navbar-->
+</div><!--container-fluid-->
+
+<div class="container-fluid">
+	<div class="banner">
+		<div class="row">
+			<div class="col-sm-12 col-xs-12">
+				<h1>Title</h1>
+			</div>
+		</div><!--row-->
+	</div><!--banner-->
+</div><!--container-fluid-->
+
+<div class="container-fluid">
+	<div class="content">
+		<div class="row">
+			<div class="col-md-3 col-sm-6 col-xs-12">
+				<img src="assets/images/12.jpg" class="port-o">
+			</div>
+				<div class="col-md-3 col-sm-6 col-xs-12">
+				<img src="assets/images/12.jpg" class="port-o">
+			</div>
+			<div class="col-md-3 col-sm-6 col-xs-12">
+				<img src="assets/images/12.jpg" class="port-o">
+			</div>
+			<div class="col-md-3 col-sm-6 col-xs-12">
+				<img src="assets/images/12.jpg" class="port-o">
+			</div>
+				
+			
+		</div><!--row-->
+	</div><!--content-->
+</div><!--container-fluid-->
+
+<div class="container-fluid">
+	<div class="footer">
+		<div class="row">
+			<div class="col-md-4 col-sm-8 col-xs-12">
+				<p>All works &copy; Riley Hein <?php echo date('Y'); ?> </p>
+			</div><!--copyright-->
+			
+			<div class="col-md-4, col-sm-8, col-xs-12">
+				<a href="#">About</a>
+				<a href="#">Resume</a>
+				<a href="#">Shop</a>
+				<a href="#">Contact</a>
+			</div><!--links-->
+			
+			<div class="col-md-4 col-sm-8 col-xs-12">
+				<img src="#"><!--insta-->
+				<img src="#"><!--youtube-->
+				<img src="#"><!--behance-->
+				<img src="#"><!--linkedin-->
+			</div><!--social-->
+			
+		</div><!--row-->
+	</div><!--footer-->
+</div><!--container-fluid-->
+
+
+<script src="js/jquery-1.11.3.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
